2358742f72981b205ef00d018c2af5e7371f7a06
Remigiusz Drobinski <30534425+curiosis@users.noreply.github.com>
2020-11-28 16:54:10 +0100

    Add background music button and sett

--- Cifre/Assets/Scripts/UIManager.cs
+++ Cifre/Assets/Scripts/UIManager.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    public GameObject canvasSett, melodyGO;
+    public GameObject canvasSett, melodyGO, melodyButtFake;
     public Sprite on, off;
 
     public void OffCanvasSett()
@@ -18,12 +19,18 @@ public class UIManager : MonoBehaviour
         if (melodyGO.activeSelf)
         {
             melodyGO.SetActive(false);
+            melodyButtFake.GetComponent<Image>().sprite = off;
         }
             
         else
         {
             melodyGO.SetActive(true);
+            melodyButtFake.GetComponent<Image>().sprite = on;
         }
-            
+    }
+
+    public void ReloadLevel()
+    {
+        SceneManager.LoadScene("Game");
     }
 }
